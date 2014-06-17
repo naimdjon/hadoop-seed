f6f48a87eed62f0e996efb4b9370b5b5c3ad81b1
Naimdjon Takhirov <naimdjon@gmail.com>
2014-06-17 23:37:07 +0200

    mrunit tests for the word count

---
+++ src/test/java/org/hadoop_seed/WordCountTest.java
@@ -0,0 +1,47 @@
+package org.hadoop_seed;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.hadoop_seed.wordcount.WordCountMapper;
+import org.hadoop_seed.wordcount.WordCountReducer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordCountTest {
+    private MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+    private ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+
+    @Before
+    public void setUp() {
+        WordCountMapper mapper = new WordCountMapper();
+        WordCountReducer reducer = new WordCountReducer();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+    }
+
+    @Test
+    public void testMapper() throws IOException {
+        mapDriver.withInput(new LongWritable(), new Text("lorem ipsum dolor ipsum"));
+        mapDriver.withOutput(new Text("lorem"), new IntWritable(1));
+        mapDriver.withOutput(new Text("ipsum"), new IntWritable(1));
+        mapDriver.withOutput(new Text("dolor"), new IntWritable(1));
+        mapDriver.withOutput(new Text("ipsum"), new IntWritable(1));
+        mapDriver.runTest();
+    }
+
+    @Test
+    public void testReducer() throws IOException {
+        final List<IntWritable> values = new ArrayList<IntWritable>(Arrays.asList(new IntWritable(1),new IntWritable(1)));
+        reduceDriver.withInput(new Text("lorem"), values);
+        reduceDriver.withOutput(new Text("lorem"), new IntWritable(2));
+        reduceDriver.runTest();
+    }
+}
